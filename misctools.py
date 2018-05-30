de5e983a96679fcdd188c0d0a2e426a700ae4645
YuShu Chen <lazyeagles@gmail.com>
2018-05-30 10:35:57 +0800

    Compute path delivery probatility

--- misctools.py
+++ misctools.py
@@ -4,6 +4,8 @@
 """
 
 import csv
+import functools
+import operator
 from typing import Tuple
 
 import numpy as np
@@ -52,3 +54,53 @@ def calculate_distance(latlon1: Latlon, latlon2: Latlon) -> np.ndarray:
     distance = earth_radius * delta_rho
 
     return distance
+
+def calculate_path_delivery_probability(
+    calculated_probs, path, link_probs, repeat
+):
+    """
+    example:
+       0.5      0.5      0.7
+    0 -----> 1 -----> 2 ----> 3
+    path = (0,1,2,3)
+    link_probs = [1,0.5,0.5,0.7,0]
+
+    """
+    if repeat in calculated_probs[path]:
+        return calculated_probs[path][repeat]
+
+    if repeat < 1 or not path or len(set(path)) < len(path):
+        return 0
+
+    if len(path) == 1:
+        calculated_probs[path][repeat] = 1.0
+        return 1.0
+
+    if repeat == 1:
+        prob = functools.reduce(
+            operator.mul, link_probs[:-1], 1
+        )
+        calculated_probs[path][repeat] = prob
+        return prob
+
+    probs = []
+    for i in range(1,len(path)+1):
+        prob1 = calculate_path_delivery_probability(
+            calculated_probs,
+            path[:i],
+            link_probs[:i] + [0],
+            1
+        )
+        prob2 = 1 - link_probs[i]
+        prob3 = calculate_path_delivery_probability(
+            calculated_probs,
+            path[i-1:],
+            [1] + link_probs[i:],
+            repeat-1
+        )
+        probs.append(prob1*prob2*prob3)
+    prob = sum(probs)
+    calculated_probs[path][repeat] = prob
+
+    return prob
+
